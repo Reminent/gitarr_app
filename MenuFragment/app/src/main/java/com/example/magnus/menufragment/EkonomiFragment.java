48c7fbb72da84a66d23da2325681029dda9ce570
Magnus Alkroth <alkrothmagnus@gmail.com>
2016-02-22 13:13:44 +0100

    Template_grupp1
    Utgå från denna template
    /Magnus

---
+++ MenuFragment/app/src/main/java/com/example/magnus/menufragment/EkonomiFragment.java
@@ -0,0 +1,16 @@
+package com.example.magnus.menufragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EkonomiFragment extends android.support.v4.app.Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.ekonomi_fragment, container, false);
+        return view;
+    }
+}
---
+++ MenuFragment/app/src/main/java/com/example/magnus/menufragment/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.magnus.menufragment;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Menu menu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.content, new SchemaFragment()).commit();
+            setTitle("Schema");
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        Fragment fragment;
+        FragmentTransaction fm = getSupportFragmentManager().beginTransaction();
+
+        if (id == R.id.nav_schema) {
+            fragment = new SchemaFragment();
+            fm.replace(R.id.content, fragment);
+            fm.commit();
+            setTitle(item.getTitle());
+        }
+        else if (id == R.id.nav_ekonomi) {
+            fragment = new EkonomiFragment();
+            fm.replace(R.id.content, fragment);
+            fm.commit();
+            setTitle(item.getTitle());
+        }
+        else if (id == R.id.nav_lager) {
+            fragment = new LagerFragment();
+            fm.replace(R.id.content, fragment);
+            fm.commit();
+            setTitle(item.getTitle());
+        }
+        else if (id == R.id.nav_annons) {
+            fragment = new AnnonsFragment();
+            fm.replace(R.id.content, fragment);
+            fm.commit();
+            setTitle(item.getTitle());
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
