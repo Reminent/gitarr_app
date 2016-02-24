bc34ef730ec2aa2f6bf55262e18f414799e93db9
Ellstrom <jonathan.ellstrom@hotmail.com>
2016-02-24 18:46:05 +0100

    Gjort en knapp
    Gjort en knapp som ändrar strängen Titel på annons som test för att
    kunna gå vidare och visa ett fragment av formuläret senare.

--- MenuFragment/app/src/main/java/com/example/magnus/menufragment/AnnonsFragment.java
+++ MenuFragment/app/src/main/java/com/example/magnus/menufragment/AnnonsFragment.java
@@ -5,12 +5,30 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class AnnonsFragment extends android.support.v4.app.Fragment {
+public class AnnonsFragment extends android.support.v4.app.Fragment implements View.OnClickListener {
     @Nullable
+
+    View view;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.annons_fragment, container, false);
+        view = inflater.inflate(R.layout.annons_fragment, container, false);
+        Button btn = (Button)view.findViewById(R.id.skapany);
+        btn.setOnClickListener(this);
+
         return view;
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.skapany:
+                ((TextView)view.findViewById(R.id.annons_titel_1)).setText("Supe du klicke på knapp!");
+                break;
+        }
     }
 }
