9cd8013ebc769b8f2ccabf5361d7b96b2dafd4e2
Ellstrom <jonathan.ellstrom@hotmail.com>
2016-02-25 16:16:13 +0100

    Ändrat formulär
    Ändrat lite allt möjligt på formulär, bland annat lagt in två bilder.
    Försökt med att få till kamerafunktionen men det har gått åt helvete.

--- MenuFragment/app/src/main/java/com/example/magnus/menufragment/AnnonsFormularFragment.java
+++ MenuFragment/app/src/main/java/com/example/magnus/menufragment/AnnonsFormularFragment.java
@@ -1,17 +1,11 @@
 package com.example.magnus.menufragment;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.DialogInterface;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * Created by Jonathan on 2016-02-24.
- */
 public class AnnonsFormularFragment extends android.support.v4.app.Fragment {
 
     @Override
@@ -19,5 +13,4 @@ public class AnnonsFormularFragment extends android.support.v4.app.Fragment {
         View view = inflater.inflate(R.layout.annons_formular_fragment, container, false);
         return view;
     }
-
 }
