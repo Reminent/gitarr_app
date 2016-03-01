393d2906657c5fda36d9dd685580e8bb2d436f4b
Ellstrom <jonathan.ellstrom@hotmail.com>
2016-03-01 14:24:52 +0100

    Kamerafunktionen
    Gjort så att när man klickar på kameraknappen så får man upp mobilens
    inbyggda kamera, och man kan ta en bild som automatiskt sparas i
    galleriet.

--- MenuFragment/app/src/main/java/com/example/magnus/menufragment/AnnonsFormularFragment.java
+++ MenuFragment/app/src/main/java/com/example/magnus/menufragment/AnnonsFormularFragment.java
@@ -1,8 +1,15 @@
 package com.example.magnus.menufragment;
 
 
-import android.hardware.Camera;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -13,10 +20,19 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class AnnonsFormularFragment extends android.support.v4.app.Fragment implements View.OnClickListener{
     @Nullable
+
+    private static String logtag = "CameraApp8";
+    private static int TAKE_PICTURE = 1;
+    private Uri imageUri;
+
 
     View view;
     @Override
@@ -28,12 +44,58 @@ public class AnnonsFormularFragment extends android.support.v4.app.Fragment impl
         AvbrytBtn.setOnClickListener(this);
 
         ImageButton kameraBtn = (ImageButton)view.findViewById(R.id.kamerasymbol);
-        kameraBtn.setOnClickListener(this);
+        kameraBtn.setOnClickListener(cameraListener);
 
         ImageButton galleriBtn = (ImageButton)view.findViewById(R.id.gallerisymbol);
         galleriBtn.setOnClickListener(this);
+
         return view;
     }
+    private View.OnClickListener cameraListener = new View.OnClickListener(){
+        public void onClick(View v){
+            takePhoto(v);
+        }
+    };
+
+    private void takePhoto(View v){
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        File photo = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"picture.jpg"); //change so it will not override picture.jpg
+        imageUri = Uri.fromFile(photo);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, TAKE_PICTURE); //We are passing in number 1, which is a request code  == main camera.
+    }
+
+    //Osäkert om det funkar, kan inte göra som vanligt eftersom att man inte kan extenda två klasser (extends Activity saknas) Se rad (***)
+    Context context;
+    public void ContactManager (Context context) {
+        this.context = context;
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent){ //should be protected, but that doesnt work.
+        onActivityResult(requestCode, resultCode, intent); //super.
+
+        if(resultCode == Activity.RESULT_OK){
+            Uri selectedImage = imageUri;
+            context.getContentResolver().notifyChange(selectedImage, null); //*** "contect." var inte med i guiden.
+
+            ImageView imageView = (ImageView)view.findViewById(R.id.image_camera); //changed, view.
+            ContentResolver cr = context.getContentResolver(); //changed, context.
+            Bitmap bitmap;
+
+            try{
+
+                bitmap = MediaStore.Images.Media.getBitmap(cr, selectedImage);
+                imageView.setImageBitmap(bitmap);
+                Toast.makeText(context, selectedImage.toString(), Toast.LENGTH_LONG).show(); //ändrad //context -> mainactivity
+            }catch (Exception e){
+                Log.e(logtag, e.toString());
+            }
+        }
+
+    }
+
 
     @Override
     public void onClick(View v) {
