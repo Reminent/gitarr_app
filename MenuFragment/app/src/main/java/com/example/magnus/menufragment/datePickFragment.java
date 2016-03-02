908d146b43826968a91c5f408d1727c17315ed43
Albinn <alst1402@student.miun.se>
2016-03-02 15:00:59 +0100

    Set date fragment
    började på att sätta en date roller för att enkelt välja datum

---
+++ MenuFragment/app/src/main/java/com/example/magnus/menufragment/datePickFragment.java
@@ -0,0 +1,40 @@
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+
+
+import com.example.magnus.menufragment.R;
+
+import java.util.Calendar;
+
+public class datePickFragment extends android.support.v4.app.Fragment implements View.OnClickListener {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    @Nullable
+    View view;
+
+    @Override
+   public Dialog onCrerateDialog(Bundle savedInstanceState){
+    final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day){
+
+
+
+    }
+}
--- MenuFragment/app/src/main/java/com/example/magnus/menufragment/nyUtgiftFragment.java
+++ MenuFragment/app/src/main/java/com/example/magnus/menufragment/nyUtgiftFragment.java
@@ -1,6 +1,5 @@
 package com.example.magnus.menufragment;
 
-
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,46 +9,44 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-public class EkonomiFragment extends android.support.v4.app.Fragment implements View.OnClickListener{
 
+public class nyUtgiftFragment extends android.support.v4.app.Fragment implements View.OnClickListener {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     @Nullable
-
     View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.ekonomi_fragment, container, false);
-        Button btnI = (Button)view.findViewById(R.id.nyinkomst);
-        btnI.setOnClickListener(this);
-        Button btnU = (Button)view.findViewById(R.id.nyutgift);
-        btnU.setOnClickListener(this);
+        View view = inflater.inflate(R.layout.fragment_ny_utgift, container, false);
+        Button btn = (Button)view.findViewById(R.id.btnStop);
+        btn.setOnClickListener(this);
         return view;
     }
 
     @Override
     public void onClick(View v) {
+        //Fragment fragment;
+        //FragmentTransaction fm = getFragmentManager().beginTransaction();
         Fragment fragment;
         FragmentTransaction fm = getFragmentManager().beginTransaction();
 
         switch(v.getId()){
-            case R.id.nyinkomst:
+            case R.id.btnStop:
+                fragment = new EkonomiFragment();
+                fm.replace(R.id.content, fragment);
+                fm.commit();
                 //((TextView)view.findViewById(R.id.annons_titel_1)).setText("Supe du klicke på knapp!");
 
                 //int id = item.getItemId();
-                fragment = new nyInkomstFragment();
-                fm.replace(R.id.content, fragment);
-                fm.commit();
+                //fragment = new nyInkomstFragment();
+                //fm.replace(R.id.content, fragment);
+                //fm.commit();
                 //setTitle(item.getTitle());
                 //fm.addToBackStack(this); //Kan vara bra för när man ska stänga formuläret./koppla formuläret mot annonssidan.
 
                 break;
-            case R.id.nyutgift:
-
-                fragment = new nyUtgiftFragment();
-                fm.replace(R.id.content, fragment);
-                fm.commit();
-
-                break;
         }
     }
+
 }
