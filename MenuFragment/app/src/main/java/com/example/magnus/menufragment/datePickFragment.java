b8a5135fffcd483a546c4929d01e88be5542ca65
Albinn <alst1402@student.miun.se>
2016-03-04 13:21:16 +0100

    skit

--- MenuFragment/app/src/main/java/com/example/magnus/menufragment/datePickFragment.java
+++ MenuFragment/app/src/main/java/com/example/magnus/menufragment/datePickFragment.java
@@ -1,5 +1,8 @@
+package com.example.magnus.menufragment;
+
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,20 +14,20 @@ import android.widget.Button;
 import android.widget.DatePicker;
 
 
-
+import com.example.magnus.menufragment.EkonomiFragment;
 import com.example.magnus.menufragment.R;
 
 import java.util.Calendar;
 
-public class datePickFragment extends android.support.v4.app.Fragment implements View.OnClickListener {
+public static class datePickFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER s
-    @Nullable
-    View view;
+    //android.support.v4.app.Fragment
+    // View view;
 
     @Override
-   public Dialog onCrerateDialog(Bundle savedInstanceState){
-    final Calendar c = Calendar.getInstance();
+    protected Dialog onCrerateDialog(Bundle savedInstanceState){
+        final Calendar c = Calendar.getInstance();
         int year = c.get(Calendar.YEAR);
         int month = c.get(Calendar.MONTH);
         int day = c.get(Calendar.DAY_OF_MONTH);
@@ -33,8 +36,7 @@ public class datePickFragment extends android.support.v4.app.Fragment implements
     }
 
     public void onDateSet(DatePicker view, int year, int month, int day){
-
-
-
+        return;
     }
+
 }
