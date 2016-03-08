b0fbab1f9f5668f18ac46551357645789c4e057f
Mattias Sundling <masu1402@student.miun.se>
2016-03-08 10:00:19 +0100

    Start of DB get
    Started implementing a DB get for class LagerFragment

--- MenuFragment/app/src/main/java/com/example/magnus/menufragment/LagerFragment.java
+++ MenuFragment/app/src/main/java/com/example/magnus/menufragment/LagerFragment.java
@@ -1,5 +1,8 @@
 package com.example.magnus.menufragment;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -13,4 +16,24 @@ public class LagerFragment extends android.support.v4.app.Fragment {
         View view = inflater.inflate(R.layout.lager_fragment, container, false);
         return view;
     }
+
+    button1.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String stringUrl = "http://spaaket.no-ip.org:1080/GitarrAppAPI/webresources/rest.transactionproduct";
+
+            ConnectivityManager connMgr = (ConnectivityManager)
+                    getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+            if (networkInfo != null && networkInfo.isConnected()) {
+                try{
+                    EkoGet task = new EkoGet();
+                    task.execute(stringUrl);
+                } catch(Throwable e){
+                }
+            } else {
+                textView.setText("No network connection available.");
+            }
+        }
+    });
 }
