bb0716434d5fe67aea6364dc12611742dc73f0d8
Mattias Sundling <masu1402@student.miun.se>
2016-03-11 12:15:16 +0100

    Minor adaptations to Lager

--- MenuFragment/app/src/main/java/com/example/magnus/menufragment/LagerAdapter.java
+++ MenuFragment/app/src/main/java/com/example/magnus/menufragment/LagerAdapter.java
@@ -1,14 +1,21 @@
 package com.example.magnus.menufragment;
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.magnus.menufragment.XML_Parsing.Advert;
 import com.example.magnus.menufragment.XML_Parsing.Product;
+
+import java.util.List;
 
 /**
  * Created by Mattias on 3/8/2016.
@@ -16,46 +23,89 @@ import com.example.magnus.menufragment.XML_Parsing.Product;
 public class LagerAdapter extends ArrayAdapter<Product>{
 
     Context context;
-    int resource;
-    Product objects[] = null;
+    int layoutResourceId;
+    private List<Product> data;
 
-    public LagerAdapter(Context context, int resource, Product[] objects) {
-        super(context, resource, objects);
+    static class LagerHolder
+    {
+        ImageView imgIcon;
+        TextView txtName;
+        TextView txtAmount;
+        Button remove;
+        Button change;
+    }
+
+    public LagerAdapter(Context context, int layoutResourceId, List<Product> data) {
+        super(context, layoutResourceId, data);
+        this.layoutResourceId = layoutResourceId;
         this.context = context;
-        this.resource = resource;
-        this.objects = objects;
+        this.data = data;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent){
+    public View getView(final int position, View convertView, ViewGroup parent) {
         View row = convertView;
-        ProductHolder holder = null;
+        LagerHolder holder = null;
 
-        if(row == null){
+        if(row == null)
+        {
             LayoutInflater inflater = ((Activity)context).getLayoutInflater();
-            row = inflater.inflate(resource, parent, false);
+            row = inflater.inflate(layoutResourceId, parent, false);
 
-            holder = new ProductHolder();
-            holder.image = (ImageView)row.findViewById(R.id.product_image);
-            holder.title = (TextView)row.findViewById(R.id.product_title);
+            holder = new LagerHolder();
+            holder.imgIcon = (ImageView)row.findViewById(R.id.product_image);
+            holder.txtName = (TextView)row.findViewById(R.id.product_name);
+            holder.txtAmount = (TextView)row.findViewById(R.id.product_amount);
+            holder.change = (Button)row.findViewById(R.id.product_edit_button);
+            holder.remove = (Button)row.findViewById(R.id.product_delete_button);
 
             row.setTag(holder);
         }
         else
         {
-            holder = (ProductHolder)row.getTag();
+            holder = (LagerHolder)row.getTag();
         }
 
-        Product product = objects[position];
-        //holder.image.setImageResource(product.getImageId());
-        holder.title.setText(product.getProductName());
+        final Product product = data.get(position);
+
+        holder.txtName.setText(product.getProductName());
+        holder.txtName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog alertDialog = new AlertDialog.Builder(getContext()).create();
+                alertDialog.setTitle("Om produkten");
+                alertDialog.setMessage(product.getGenre());
+
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+            }
+        });
+
+        //holder.imgIcon.setImageResource(advert.icon);
+        //holder.imgIcon.setImageResource(advert.getImageid()); //TODO: fix this so we can fetch images from db
+
+        holder.change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Redigera annons nr." + position, Toast.LENGTH_LONG).show();
+                //TODO: Change this so it changes the database instead.
+            }
+        });
+
+        holder.remove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Ta bort annons nr." + position, Toast.LENGTH_LONG).show();
+                //TODO: Change this so it deletes an item in the database instead.
+            }
+        });
 
         return row;
-    }
 
-    static class ProductHolder
-    {
-        ImageView image;
-        TextView title;
     }
 }
--- MenuFragment/app/src/main/java/com/example/magnus/menufragment/LagerFragment.java
+++ MenuFragment/app/src/main/java/com/example/magnus/menufragment/LagerFragment.java
@@ -5,44 +5,49 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.magnus.menufragment.DB_Connect.DB_Connect;
+import com.example.magnus.menufragment.XML_Parsing.Advert;
+import com.example.magnus.menufragment.XML_Parsing.Advert_Parse;
 import com.example.magnus.menufragment.XML_Parsing.Product;
 import com.example.magnus.menufragment.XML_Parsing.Product_Parse;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LagerFragment extends android.support.v4.app.Fragment {
 
-    List<Product> products = null;
+    private ListView myListView;
+    private List<Product> products = new ArrayList<>();
+    private View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.lager_fragment, container ,false);
+        view = inflater.inflate(R.layout.lager_fragment, container ,false);
         LagerGet lagerGet = new LagerGet();
         lagerGet.execute("http://spaaket.no-ip.org:1080/GitarrAppAPI/webresources/rest.product");
 
         return view;
     }
 
-    private class LagerGet extends DB_Connect {
 
-/*
-    button1.setOnClickListener(new View.OnClickListener() {
->>>>>>> origin/Annons-Mattias
+    private class LagerGet extends DB_Connect {
         @Override
         protected void onPostExecute(String result) {
-            //List<Product> products = null;
+
             try {
                 Product_Parse parser = new Product_Parse();
                 products = parser.parse(result);
+
+                LagerAdapter adapter = new LagerAdapter(getContext(),R.layout.lager_item_row, products);
+                myListView = (ListView)view.findViewById(R.id.lagerListView);
+                myListView.setAdapter(adapter);
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-}
-    });
-    */
     }
+}
